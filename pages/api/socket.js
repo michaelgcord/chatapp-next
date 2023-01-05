7eea0633cccf196a39181d97d13e5e20ea7b56e0
Michael Cordero <michaelcordero@michaels-mbp.lan>
2023-01-05 01:24:37 -0800

    first commit

---
+++ pages/api/socket.js
@@ -0,0 +1,20 @@
+import { Server } from 'Socket.IO'
+
+const SocketHandler = (req, res) => {
+  if (res.socket.server.io) {
+    console.log('Socket is already running')
+  } else {
+    console.log('Socket is initializing')
+    const io = new Server(res.socket.server)
+    res.socket.server.io = io
+
+    io.on('connection', socket => {
+      socket.on('input-sent', msg => {
+        socket.broadcast.emit('input-receive', msg)
+      })      
+    })
+  }
+  res.end()
+}
+
+export default SocketHandler
